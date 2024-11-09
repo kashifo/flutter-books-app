68acc589a3986f307954e7e98571b3d8314b6391
kashifo <koderkashif@gmail.com>
2024-11-09 14:13:57 +0530

    Worked on Login & Signup. - Show/hide fields based on login or signup - Converted to form

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../main.dart';
 import '../utils/commons.dart';
 
 class Login extends StatefulWidget {
@@ -11,101 +12,180 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   bool obscureText = true;
+  bool isSignupScreen = false;
+  final formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: getIntColor('0065ff'),
       backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          shrinkWrap: true,
-          children: [
+      body: Form(
+        key: formKey,
+        child: Center(
+          child: ListView(
+            padding: EdgeInsets.all(16),
+            shrinkWrap: true,
+            children: [
 
-            Image.asset(
-              'assets/icons/app_icon_512.png',
-              width: 100,
-              height: 100,
-            ),
+              Image.asset(
+                'assets/icons/app_icon_512.png',
+                width: 100,
+                height: 100,
+              ),
 
-            SizedBox(
-              height: 25,
-            ),
+              SizedBox(
+                height: 25,
+              ),
 
-            TextField(
-              keyboardType: TextInputType.name,
-              textInputAction: TextInputAction.next,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(16),
-                hintText: 'E-Mail',
-                hintStyle: TextStyle(color: Colors.white38),
-                prefixIcon: Icon(Icons.email, color: Colors.white,),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.white38, width: 1.0, style: BorderStyle.solid)
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.white, width: 1.0, style: BorderStyle.solid)
+              Visibility(
+                visible: isSignupScreen,
+                child: TextFormField(
+                  controller: nameController,
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(16),
+                    prefixIcon: Icon(Icons.person, color: Colors.white,),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white38, width: 1.0, style: BorderStyle.solid)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 1.0, style: BorderStyle.solid)
+                    ),
+                    labelText: "Name",
+                    labelStyle: TextStyle(color: Colors.white54),
+                    floatingLabelStyle: TextStyle(color: Colors.white),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
                 ),
               ),
-            ),
 
-            SizedBox(
-              height: 16,
-            ),
+              SizedBox(
+                height: 16,
+              ),
 
-            TextField(
-              obscureText: obscureText,
-              keyboardType: TextInputType.visiblePassword,
-              textInputAction: TextInputAction.done,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
                   contentPadding: EdgeInsets.all(16),
-                  hintText: 'Password',
-                  hintStyle: TextStyle(color: Colors.white38),
-                  prefixIcon: Icon(Icons.key, color: Colors.white,),
+                  prefixIcon: Icon(Icons.email, color: Colors.white,),
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(color: Colors.white38, width: 1.0, style: BorderStyle.solid)
+                      borderSide: BorderSide(color: Colors.white38, width: 1.0, style: BorderStyle.solid),
                   ),
                   focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide(color: Colors.white, width: 1.0, style: BorderStyle.solid)
                   ),
-                  suffixIcon: IconButton(onPressed: (){
-                    setState(() {
-                      obscureText = !obscureText;
-                    });
-                  }, icon: Icon( obscureText ? Icons.remove_red_eye : Icons.remove_red_eye_outlined, color: Colors.white, ))
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Colors.white54),
+                  floatingLabelStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
               ),
-            ),
 
-            SizedBox(height: 16),
+              SizedBox(
+                height: 16,
+              ),
 
-            MaterialButton(
-              height: 50,
-              color: Colors.white,
-              textColor: getIntColor('0065ff'),
-              onPressed: () => {
+              TextFormField(
+                controller: passwordController,
+                obscureText: obscureText,
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(16),
+                    prefixIcon: Icon(Icons.key, color: Colors.white,),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white38, width: 1.0, style: BorderStyle.solid)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.white, width: 1.0, style: BorderStyle.solid)
+                    ),
+                    suffixIcon: IconButton(onPressed: (){
+                      setState(() {
+                        obscureText = !obscureText;
+                      });
+                    }, icon: Icon( obscureText ? Icons.remove_red_eye : Icons.remove_red_eye_outlined, color: Colors.white, )),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.white54),
+                    floatingLabelStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
 
-              },
-              child: Text('LOGIN', style: TextStyle(fontWeight: FontWeight.bold),),
-            ),
+              SizedBox(height: 16),
 
-            SizedBox(height: 16),
-
-            Text(
-              'Sign Up',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+              MaterialButton(
+                height: 50,
                 color: Colors.white,
+                textColor: getIntColor('0065ff'),
+                onPressed: () => {
+                  if(formKey.currentState!.validate()){
+
+                    if(emailController.text=="koderkashif@gmail.com" && passwordController.text=="qwerty"){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => TabView() ))
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Invalid credentials'))
+                      )
+                    }
+
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Please fill in the details'),)
+                    )
+                  }
+                },
+                child: Text(isSignupScreen ? 'SIGN UP' : 'LOGIN', style: TextStyle(fontWeight: FontWeight.bold),),
               ),
-            ),
-          ],
+
+              SizedBox(height: 16),
+
+              InkWell(
+                onTap: (){
+                  setState(() {
+                    isSignupScreen = !isSignupScreen;
+                  });
+                },
+                child: Text(
+                  isSignupScreen ? 'Already have an account? Login' : 'Don\'t have an account? Sign Up',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
